72913657813c8d2560559e7b0a7d7b234a073c0f
AVN <avnesterov88@gmail.com>
2022-01-25 16:58:05 +0700

    r5.88
    app component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// const elem = <h2>"Hello World"</h2>;
-
-const text = "Hello World!!!";
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text"/>
-    <button tabIndex='0'>Click</button>
-  </div>
-  );
-
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
