322bd55b67ef8fedd067267b3ada2663d0bc5526
AVN <avnesterov88@gmail.com>
2022-01-28 15:52:22 +0700

    r5.88
    elements and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+class Field extends Component {
+  render() {
+      const holder = 'Enter here';
+      const styledField = {
+      width: '300px'
+    };  
+    return <input 
+            style={styledField} 
+            placeholder={holder} 
+            type="text"/> 
+  }
+}
+
+function Btn() {
+  const text = 'Log In';
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Field/>
+      <Btn/> 
     </div>
   );
 }
 
+export {Header};
 export default App;
